2d72fbd62913eb0d61aea3d09e1b71a85749e041
Aditya.K <kunchaaditya369@gmail.com>
2025-03-05 19:52:58 +0530

    CA completed

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -10,21 +10,35 @@ const TodoList = () => {
   // Function to handle the input change
   const handleInputChange = (e) => {
     // Code and complete the logic to update the newTodo state with the value from the input field
+    setNewTodo(e.target.value)
   };
 
   // Function to add a new todo
   const handleAddTodo = () => {
     //  Code and complete the logic to add the new todo if it's not empty
+    if(todos!==''){
+      setTodos([...todos,{text:newTodo,completed:false}])
+      console.log(todos)
+      
+    }
+    setNewTodo("")
   };
 
   // Function to toggle the completion status of a todo
   const handleToggleComplete = (index) => {
     // Code and Complete the logic to toggle the 'completed' status of the selected todo
+    todos.map((todo,i)=>{
+      if(i==index){
+        todo.completed=true
+      }
+    })
   };
 
   // Function to delete a todo
   const handleDeleteTodo = (index) => {
     //  Code and complete the logic to remove the selected todo from the list
+    const filtered = todos.filter((todo,i)=>i!==index)
+    setTodos(filtered)
   };
 
   return (
@@ -34,6 +48,7 @@ const TodoList = () => {
         type="text"
         placeholder="Add a new task"
         // Ensure this input field is controlled via newTodo state
+        value={newTodo}
         onChange={handleInputChange}
       />
       <button onClick={handleAddTodo}>Add Todo</button>
